aa5d212034003b66990699d968d7adf3eeca0f0e
Antonenko Aleksandr <114353007+daates@users.noreply.github.com>
2024-12-27 03:25:41 +0300

    Update style home page and login page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < ApplicationController
   def index
-    render html: "<h1>Welcome to Booking Platform</h1><a href='#{login_path}'>Login</a> or <a href='#{new_user_path}'>Sign Up</a>".html_safe
+    #render html: "<h1>Welcome to Booking Platform</h1><a href='#{login_path}'>Login</a> or <a href='#{new_user_path}'>Sign Up</a>".html_safe
   end
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # def error_message_for(object, field, options = {})
+  #   if object.errors[field].present?
+  #     content_tag(:div, object.errors[field].first, options)
+  #   end
+  # end
 end
